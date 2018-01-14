8e19f2a684b0fd0cc464c0d195f1f24595cfab5c
Adam Neumann <noizwaves@users.noreply.github.com>
2018-01-13 17:16:43 -0700

    Update Tandem.app to v0.3.2

--- Casks/tandem.rb
+++ Casks/tandem.rb
@@ -1,6 +1,6 @@
 cask 'tandem' do
-  version '0.3.1'
-  sha256 'ede97a533cb36eac5f268186f2f52528b9694d2b016ac1fde2eb947180d3938e'
+  version '0.3.2'
+  sha256 '4942ab72f230c38b54e83045691cf1d36eda8f46f6c62c444dd48ef3b3bd17df'
 
   url "https://github.com/RideTandem/tandem-releases/releases/download/v#{version}/Tandem-#{version}.dmg"
   name 'Tandem'
